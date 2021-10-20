be9df70ef2c1214dd89e67751ff745e919032874
GHOST <ghostdevbusiness@gmail.com>
2021-10-21 00:51:40 +0100

    refactor: body component should use actions

--- src/Body.svelte
+++ src/Body.svelte
@@ -1,42 +1,11 @@
 <script>
-    import { onMount } from 'svelte';
-
-    // Create pseudo html element, the parser for styles is amazing
-    const pseudoElement = document.createElement('div');
+    import { classList, style as styleAction } from './actions';
 
     export let style;
 
-    // We can't use class as a prop directly sine it's a keyword in JS, so we do a work around and split it by a space since class names can't have a space in them
-    const classes = ($$props?.class || '')
-        .split(' ')
-        .filter((x) => x.trim() != '');
-
-    // Give the pseudo element our style string, it can handle this
-    if (typeof style == 'string') pseudoElement.style = style;
-
-    // For each property in the styles obect apply it to the elemnt
-    if (typeof style == 'object')
-        for (const [property, value] of Object.entries(style))
-            pseudoElement.style[property] = value;
-
-    onMount(() => {
-        // Combine the existing styles on the body and the computed style string on the pseudo element and apply to body
-        document.body.style = `
-					${document.body.style.cssText};
-					${pseudoElement.style.cssText};
-				`;
-
-        // Apply given classes
-        if (classes?.length) document.body.classList.add(...classes);
-
-        return () => {
-            // When the component is destroyed remove the styles that we applied on mount
-            document.body.style = document.body.style.cssText.replace(
-                pseudoElement.style.cssText,
-            );
-
-            // Remove classes
-            if (classes?.length) document.body.classList.remove(...classes);
-        };
-    });
+    // Get around not being able to use class reserved word
+    const classes = $$props?.class || '';
 </script>
+
+<!-- Use actions to avoid code duplication and make svelte-body more reactive -->
+<svelte:body use:classList={classes} use:styleAction={style} />
