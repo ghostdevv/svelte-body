298c5d33a8c1ab1598edb7bfa87f7f728ce9f845
Habib Mustofa <hm.dev7@gmail.com>
2021-10-23 13:26:10 +0700

    Define class prop

--- src/Body.svelte
+++ src/Body.svelte
@@ -1,10 +1,9 @@
 <script>
     import { classList, style as styleAction } from './actions';
 
+    let classes = ''
+    export { classes as class }
     export let style;
-
-    // Get around not being able to use class reserved word
-    const classes = $$props?.class || '';
 </script>
 
 <!-- Use actions to avoid code duplication and make svelte-body more reactive -->
