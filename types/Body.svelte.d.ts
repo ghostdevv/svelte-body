0a075ee0e6fe54360752232c1961bb5ab87f932b
Puru Vijay <awesomepuruvj@gmail.com>
2021-12-05 02:02:03 +0530

    Add csstype typings to Body.svelte.d.ts

--- types/Body.svelte.d.ts
+++ types/Body.svelte.d.ts
@@ -1,16 +1,17 @@
 /// <reference types="svelte" />
-import { SvelteComponentTyped } from "svelte";
+import { SvelteComponentTyped } from 'svelte';
+import { Properties } from 'csstype';
 
 export interface BodyProps {
-  /**
-   * A string of styles (style="" on a HTML element)
-   */
-  style?: string | CSSStyleDeclaration;
+    /**
+     * A string of styles (style="" on a HTML element)
+     */
+    style?: string | Properties;
 
-  /**
-   * A string of class names (class="" on a HTML element)
-   */
-  class?: string;
+    /**
+     * A string of class names (class="" on a HTML element)
+     */
+    class?: string;
 }
 
 export default class Body extends SvelteComponentTyped<BodyProps, {}, {}> {}
