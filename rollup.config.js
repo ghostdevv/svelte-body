ee814392185e379d2755535e3571a12c751539e9
Puru Vijay <awesomepuruvj@gmail.com>
2021-12-05 12:24:49 +0530

    Commit

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
 import pkg from './package.json';
+import copy from 'rollup-plugin-copy';
 
 export default {
     input: 'src/index.js',
@@ -9,5 +10,15 @@ export default {
         { file: pkg.module, format: 'es' },
         { file: pkg.main, format: 'umd', name: 'svelte-body' },
     ],
-    plugins: [svelte({ emitCss: false }), resolve(), commonjs()],
+    plugins: [
+        svelte({ emitCss: false }),
+        resolve(),
+        commonjs(),
+        copy({
+            targets: [
+                { src: './types/*', dest: './dist' },
+                { src: './LICENSE', dest: './dist' },
+            ],
+        }),
+    ],
 };
