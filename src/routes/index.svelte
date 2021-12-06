3d6c5b98a3378ec7dace9cad7a2fe02f36b0645c
GHOSt <ghostdevbusiness@gmail.com>
2021-12-06 19:03:04 +0000

    chore: remove unused

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,6 @@
     import Body from '$lib/Body.svelte';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-
 <Body
     style={{ '--hello': 'world', '--nice': 'work' }}
     class={{ hello: true }} />
